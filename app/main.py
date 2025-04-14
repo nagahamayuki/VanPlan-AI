73f8a65c4414cd21dc67dc788599298aabb2d4a6
Yuki Nagahama <yuuki4104453@gmail.com>
2025-04-14 16:22:34 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from typing import List
 
 app = FastAPI()
 
 @app.get("/vanning/check")
-def check_vanning(vins: List[str]):
+def check_vanning(vins: List[str] = Query(...)):
     return {"message": "VINを受け取りました", "vins": vins}
